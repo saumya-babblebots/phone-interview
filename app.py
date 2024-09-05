e9b1a4dca494b36471561eb5e3c1f6c1a2fadc0f
saumya-babblebots <saumya@babblebots.ai>
2024-09-06 00:37:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,129 @@
+import streamlit as st
+import requests
+from python_dotenv import load_dotenv
+import os
+import prompts
+
+load_dotenv()
+# Your Vapi API Authorization token
+auth_token = os.getenv("VAPI_AUTH_TOKEN")
+
+# The Phone Number ID, and the Customer details for the call
+phone_number_id = os.getenv("ASSISTANT_PHONE_NUMBER_ID")
+
+
+# Create the header with Authorization token
+headers = {
+    'Authorization': f'Bearer {auth_token}',
+    'Content-Type': 'application/json',
+}
+
+def create_payload(company, questions, candidate_phone_number, candidate_name):
+    # Create the payload for the API request
+    if not candidate_name:
+        candidate_name = "there"
+    data = {
+        'assistant': {
+            "firstMessage": prompts.first_bot_message.format(company=company, candidate_name=candidate_name),
+            "endCallMessage": prompts.end_call_message,
+            "responseDelaySeconds": 1.0,
+            "silenceTimeoutSeconds": 30,
+            "transcriber": {
+                "model": "nova-2",
+                "language": "en",
+                "provider": "deepgram",
+                "keywords": [company, candidate_name]
+            },
+            "model": {
+                "provider": "openai",
+                "model": "gpt-4o-mini",
+                "maxTokens": 150,
+                "numFastTurns": 2,
+                "messages": [
+                    {
+                        "role": "system",
+                        "content": prompts.system_prompt.format(company=company),
+                        "role": "assistant",
+                        "content": prompts.user_prompt.format(questions=questions, company=company)
+                    }
+                ]
+            },
+            "endCallFunctionEnabled": True,
+            "voice":  {
+                "model": "eleven_turbo_v2_5",
+                "voiceId": "cjVigY5qzO86Huf0OWal", #"orpheus",
+                "provider": "11labs", # 'deepgram'
+                "stability": 0.5,
+                "similarityBoost": 0.75
+            },
+            "backgroundSound": "off"
+        },
+        'phoneNumberId': phone_number_id,
+        'customer': {
+            'number': candidate_phone_number,
+        },
+    }
+
+    return data
+
+
+def create_call(data):
+    # Make the POST request to VAPI to create the phone call
+    response = requests.post(
+        'https://api.vapi.ai/call/phone', headers=headers, json=data
+    )
+
+    # Check if the request was successful
+    if response.status_code == 201:
+        print('Call created successfully')
+        print(response.json())
+    else:
+        print('Failed to create call')
+        print(response.text)
+
+company = st.text_input(
+    label="Enter the name of the company that the AI assistant is calling on behalf of",
+    value="AMS"
+)
+
+candidate_name = st.text_input(
+    label="Enter the first name of the candidate here (optional)",
+    # value="AMS"
+)
+
+questions = st.text_area(
+    label="Enter the questions here",
+    height=300,
+    key="questions_text"
+)
+
+def warehouse_operator_flow_clicked():
+    st.session_state.questions_text = prompts.warehouse_operator_questions
+
+def work_history_flow_clicked():
+    st.session_state.questions_text = prompts.work_history_questions
+
+def signed_contract_flow_clicked():
+    st.session_state.questions_text = prompts.signed_contract_questions.format(company=company)
+
+st.write("You can also choose from a questions template below")
+st.button("Warehouse Operator", on_click=warehouse_operator_flow_clicked)
+st.button("Onboarding flow asking for signed contract", on_click=signed_contract_flow_clicked)
+st.button("Onboarding flow with work history questions", on_click=work_history_flow_clicked)
+
+
+phone_number = st.text_input(
+    label="Enter the candidate's phone number here in the given format",
+    placeholder="+1-888-888-8888",
+)
+
+# st.button("Make the call", type="primary")
+
+# st.write(questions)
+
+if st.button("Make the call", type="primary"):
+    data = create_payload(company, questions, phone_number, candidate_name)
+    st.write("The AI assistant is calling the above number now.")
+    # create_call(data)
+
+    # print(prompts.user_prompt.format(questions=questions))
---
+++ prompts.py
@@ -0,0 +1,77 @@
+system_prompt = """Welcome, Eric! You are the friendly, warm, and professional voice interview assistant of {company}, here to ask candidates undergoing an interview process a few routine questions. 
+Your main task is to interview through audio interactions. Remember, candidates can't see you, so your words need to paint the picture clearly and warmly.
+
+**Guidelines for the conversation:**
+1. Do not address candidate with any name, adjective or anything like this.
+2. Stick to the interview questions provided and aim to gather the necessary information efficiently.
+3. **Keep the Focus:** If the candidate strays from the interview topics, gently remind them of the interview's focus with a polite statement like, "That's an interesting question, but let's focus on your interview today so we can get through all the important details.". Politely redirect the conversation without being dismissive to ensure that the candidate remains engaged and feels respected.
+4. **Pacing:** Maintain a steady and moderate pace so candidates can easily follow your questions and move to each question one by one.
+5. **Stay Positive:** Always maintain a positive and respectful tone, ensuring the candidate feels supported.
+
+Your role is crucial in making {company}'s AI-recruiting experience outstanding. Let's make every interaction count!
+"""
+
+# - If necessary, 
+# Let's create a professional and seamless interview experience by staying on topic and ensuring every interaction is helpful and focused on the candidate's application process.
+# 2. **Keep the Focus:** If the candidate asks questions unrelated to the interview, such as non-professional topics, gently guide them back with responses like, "Let's stick to the questions related to your interview today. We'll make sure we cover everything we need to for your application."
+
+
+# questions = """
+# 1. We've noticed there is a 3-month gap in your work history for the period June to September 2023, please can you clarify what you were doing during this time?
+# 2. Would you be able to provide evidence of that?
+# 3. Would you like us to re-send the link to the portal or are you ok?
+# """
+
+user_prompt = """
+Ask the following questions to an interview candidate in sequence based on the conversation that has happened so far.
+
+Instructions:
+- Move on to the next question only when you have received a valid and complete response to your question.
+- Wherever applicable, reassure the candidate with a prompt and friendly acknowledgment to their response.
+- DO NOT make up answers. If the candidate asks something that needs you to know about company policies etc., truthfully say that you don't know and they can contact the HR regarding that.
+- DO NOT ask any other questions or offer to help the user with any other questions other than the ones below.
+- You can rephrase or repeat a question if the situation demands, but DO NOT make up a new question.
+- Politely say goodbye and end the call when all questions below have been asked.
+
+Questions: {questions}
+"""
+# - DO NOT answer any questions asked out of the context of these questions. In such a scenario, just let the candidate know that their question is out of the scope of this call and bring them back to the original conversation.
+# 2. **Feedback Queries:** At the end when all the above questions are asked, ask for feedback to confirm the candidate is satisfied with the call.
+
+first_bot_message = "Hi {candidate_name}! I'm Eric, your AI Recruiting Assistant from {company}. Can I ask you a few quick questions today?"
+end_call_message = "Thank you for your time. Have a great day!"
+
+signed_contract_questions = """
+1. We're calling as you are due to start your role with {company} on Monday, however we haven't received your signed contract. Can you confirm if you have received it via email?
+2. If candidate hasn't received it via email - ask them to check their junk mail.
+3. If it's not in the junk mail - ask if they would like you to re-send it to them.
+4. If yes, confirm the email address to send it.
+5. If they have received it, check everything is ok with it and confirm with them they will sign and return
+6. In both scenarios confirm that we need a signed copy back before Monday. If we don't receive it, they cannot start their role.
+"""
+
+work_history_questions = """
+1. We've noticed there is a 3-month gap in your work history for the period June to September 2023. Can you please clarify what you were doing during this time?
+2. Would you be able to provide evidence of that?
+3. Would you like us to re-send the link to the portal or are you ok?
+"""
+
+warehouse_operator_questions = """
+1. What made you apply for this position?
+2. Have you worked in a warehouse before?  
+3. If they have, ask how many years of experience they have working in a warehouse.
+3. Are you comfortable working in extreme heat or cold?
+4. What motivates you?
+5. Why do you feel you are a good candidate for this position?
+6. The roles and positions can change depending on production needs how flexible are you to change?
+7. We have openings on the following shifts:
+    - First Shift works Tuesday through Friday from 5:00am to 3:00pm
+    - Second Shift works Tuesday through Friday from 4:00pm until 2:00am
+    - Weekend Shift works Saturday through Monday from 5:00am to 5:00pm
+    - second shift, and weekend shift.  
+Which of these shifts are you available to work?
+
+8. If you are chosen to move forward, you must be authorized to work in the United States. And you must consent to a background screening and drug test. Do you want to move forward?
+    - If the candidate answers with a yes, respond with 'Great! We will review your responses and follow up with you soon to discuss the next steps.', and end the call.
+    - If the candidate answers with a no, respond with 'Thank you for your time.  If anything changes in the future and you change your mind, please let us know.', and end the call.
+"""
