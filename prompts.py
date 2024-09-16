6150d9c4aaf5e09279ed25cc8f785b7bb286c742
saumya-babblebots <saumya@babblebots.ai>
2024-09-17 03:23:02 +0530

    Update prompts.py for new role

--- prompts.py
+++ prompts.py
@@ -1,4 +1,7 @@
-system_prompt = """Welcome, Eric! You are the friendly, warm, and professional voice interview assistant of {company}, here to ask candidates undergoing an interview process a few routine questions. 
+first_bot_message = "Hi {candidate_name}, I’m {recruiter}, an AI-Recruiter Assistant from {company}. I’m reaching out about the {role} position and have a few questions for you. I understand this might be your first time speaking with an AI-Recruiter, so thanks for taking the time to chat with me! Shall we begin?" # Let's get started {candidate_name}."
+end_call_message = "That's all the questions I had for you today. Thank you for your time {candidate_name}, someone from our team will contact you further if you get shortlisted. Have a great day. Bye!"
+
+system_prompt = """You are the friendly, warm, and professional voice interview assistant of {company}, here to ask candidates undergoing an interview process a few routine questions. 
 Your main task is to interview through audio interactions. Remember, candidates can't see you, so your words need to paint the picture clearly and warmly.
 
 **Guidelines for the conversation:**
@@ -11,17 +14,6 @@ Your main task is to interview through audio interactions. Remember, candidates 
 Your role is crucial in making {company}'s AI-recruiting experience outstanding. Let's make every interaction count!
 """
 
-# - If necessary, 
-# Let's create a professional and seamless interview experience by staying on topic and ensuring every interaction is helpful and focused on the candidate's application process.
-# 2. **Keep the Focus:** If the candidate asks questions unrelated to the interview, such as non-professional topics, gently guide them back with responses like, "Let's stick to the questions related to your interview today. We'll make sure we cover everything we need to for your application."
-
-
-# questions = """
-# 1. We've noticed there is a 3-month gap in your work history for the period June to September 2023, please can you clarify what you were doing during this time?
-# 2. Would you be able to provide evidence of that?
-# 3. Would you like us to re-send the link to the portal or are you ok?
-# """
-
 user_prompt = """
 Ask the following questions to an interview candidate in sequence based on the conversation that has happened so far.
 
@@ -31,16 +23,27 @@ Instructions:
 - DO NOT make up answers. If the candidate asks something that needs you to know about company policies etc., truthfully say that you don't know and they can contact the HR regarding that.
 - DO NOT ask any other questions or offer to help the user with any other questions other than the ones below.
 - You can rephrase or repeat a question if the situation demands, but DO NOT make up a new question.
-- When all the below questions are asked, acknowledge and politely end the interview from your end. Do not say thankyou. Don't wait for candidate's permission to end.
+- When all the below questions are asked, acknowledge and politely end the interview by saying "That's all the questions I had for you today. Someone from our team will contact you further if you get shortlisted. Thank you for your time. Have a great day. Bye!".
 **Note** STRICTLY follow the above instructions.
 
 Questions: {questions}
 """
-# - DO NOT answer any questions asked out of the context of these questions. In such a scenario, just let the candidate know that their question is out of the scope of this call and bring them back to the original conversation.
-# 2. **Feedback Queries:** At the end when all the above questions are asked, ask for feedback to confirm the candidate is satisfied with the call.
 
-first_bot_message = "Hi {candidate_name}, I’m Eric, an AI-Recruiter Assistant from {company}. I’m reaching out about the {role} position and have a few questions for you. I understand this might be your first time speaking with an AI-Recruiter, so thanks for taking the time to chat with me! Let's get started {candidate_name}."
-end_call_message = "That's all the questions I had for you today. Thank you for your time {candidate_name}, someone from our team will contact you further if you get shortlisted. Have a great day. Bye!"
+user_prompt_with_probing = """
+Ask the following questions to an interview candidate in sequence based on the conversation that has happened so far.
+
+Instructions:
+- Move on to the next question only when you have received a valid and complete response to your question.
+- Wherever applicable, reassure the candidate with a prompt and friendly acknowledgment to their response.
+- DO NOT make up answers. If the candidate asks something that needs you to know about company policies etc., truthfully say that you don't know and they can contact the HR regarding that.
+- DO NOT offer to help the user with any other questions other than the ones below.
+- You can rephrase or repeat a question if the situation demands.
+- For any of the given questions, you are allowed to probe the user with EXACTLY one more question if their response to that question seems too short or incomplete.
+- When all the below questions are asked, acknowledge and politely end the interview by saying "That's all the questions I had for you today. Someone from our team will contact you further if you get shortlisted. Thank you for your time. Have a great day. Bye!".
+**Note** STRICTLY follow the above instructions.
+
+Questions: {questions}
+"""
 
 retail_appointment_generator = """
 1. So {first_name}, can you tell me about your most recent work experience in customer service or sales?
@@ -48,7 +51,7 @@ retail_appointment_generator = """
 3. We're looking for someone who can start in the next two weeks. Would you be available to begin then?
 """
 
-signed_contract_questions = """
+onboarding_flow_asking_for_signed_contract = """
 1. We're calling as you are due to start your role with {company} on Monday, however we haven't received your signed contract. Can you confirm if you have received it via email?
 2. If candidate hasn't received it via email - ask them to check their junk mail.
 3. If it's not in the junk mail - ask if they would like you to re-send it to them.
@@ -57,13 +60,13 @@ signed_contract_questions = """
 6. In both scenarios confirm that we need a signed copy back before Monday. If we don't receive it, they cannot start their role.
 """
 
-work_history_questions = """
+onboarding_flow_asking_for_work_history = """
 1. We've noticed there is a 3-month gap in your work history for the period June to September 2023. Can you please clarify what you were doing during this time?
 2. Would you be able to provide evidence of that?
 3. Would you like us to re-send the link to the portal or are you ok?
 """
 
-warehouse_operator_questions = """
+warehouse_operator = """
 1. What made you apply for this position?
 2. Have you worked in a warehouse before?  
 3. If they have, ask how many years of experience they have working in a warehouse.
@@ -83,7 +86,7 @@ Which of these shifts are you available to work?
     - If the candidate answers with a no, respond with 'Thank you for your time.  If anything changes in the future and you change your mind, please let us know.', and end the call.
 """
 
-sales_manager_questions = """
+channel_sales_manager = """
 1. Ask them about any direct experience selling commercial vehicles, construction, heavy machinery, engineering, etc.
 2. If they say they have no experience then say it's not a good fit and politely end the call.
 3. If they have any experience, ask the following set of questions:
@@ -97,17 +100,39 @@ sales_manager_questions = """
     - What are the meanings of the following terms (ask one by one) - Differential, Gradability, Gear Ratio?
 """
 
-software_engineer_questions = """
+software_engineer = """
 1. What programming languages do you prefer?
 2. Describe the Software Development Process in brief.
 3. What are some software engineering tools that you have used in your work?
 4. Mention some challenges of software development.
 """
 
-nurse_practitioner_questions = """
+nurse_practitioner = """
 1. What were the most challenging aspects of your training, and how did you overcome them?
 2. Can you recall a specific patient case that had a significant impact on your professional growth?
 3. How have your academic and clinical experiences prepared you for this role in today's healthcare environment?
 4. Can you recall an instance when you applied evidence-based guidelines effectively?
 5. Have you leveraged feedback from peers or superiors to improve patient care?
 """
+
+car_salesman = """
+1. What has been your revenue target and achievement in the past year?
+2. How many new customers have you brought on-board and what has been the average value of machines sold?
+3. What is the market share of your automobiles in your area and what is the market share of your competitors?
+4. What is the cost difference between your company products and that of the competitors?
+5. What is the functional USP of your product that sets it apart from the competitors?
+6. What was the retention rate of team members?
+7. Number of training sessions or engagements conducted for dealership team monthly?
+8. What are the meanings of the following - Differential, Gradability, Gear Ratio?
+"""
+
+interview_questions = {
+    "retail_appointment_generator": retail_appointment_generator,
+    "warehouse_operator": warehouse_operator,
+    "channel_sales_manager": channel_sales_manager,
+    "nurse_practitioner": nurse_practitioner,
+    "software_engineer": software_engineer,
+    "onboarding_flow_asking_for_work_history": onboarding_flow_asking_for_work_history,
+    "onboarding_flow_asking_for_signed_contract": onboarding_flow_asking_for_signed_contract,
+    "car_salesman": car_salesman
+}
