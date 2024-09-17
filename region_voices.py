75c73e858faa5f9ed51cded2325362b5b43778d9
saumya-babblebots <saumya@babblebots.ai>
2024-09-17 14:13:02 +0530

    Updated region_voices.py

--- region_voices.py
+++ region_voices.py
@@ -2,14 +2,14 @@
 REGION_VOICES = {
     "US": {
         "model": "eleven_turbo_v2_5",
-        "voiceId": "cjVigY5qzO86Huf0OWal",  # Example US voice ID
+        "voiceId": "cjVigY5qzO86Huf0OWal",  
         "provider": "11labs",
         "stability": 0.5,
         "similarityBoost": 0.75
     },
     "India": {
         #"model": "eleven_turbo_v2_5",
-        "voiceId": "en-IN-NeerjaNeural",  # Replace with actual Indian voice ID
+        "voiceId": "en-IN-NeerjaNeural",  
         "provider": "azure",
         #"stability": 0.5,
         #"similarityBoost": 0.75
