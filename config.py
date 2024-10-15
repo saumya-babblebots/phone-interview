35672d225a5ab9b088e322860c5453514ace69af
saumya-babblebots <saumya@babblebots.ai>
2024-10-15 07:46:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import prompts
 stt_model = {
     "model": "nova-2",
     "language": "en",
-    "provider": "deepgram"
+    "provider": "deepgram",
+    "keywords": []
 }
 
 llm = {
@@ -21,3 +22,75 @@ llm = {
         }
     ]
 }
+
+REGION_VOICES = {
+    "US": {
+        "model": "eleven_turbo_v2_5",
+        "voiceId": "cjVigY5qzO86Huf0OWal",  
+        "provider": "11labs",
+        "stability": 0.5,
+        "similarityBoost": 0.75
+    },
+    "India": {
+        #"model": "eleven_turbo_v2_5",
+        "voiceId": "en-IN-AnanyaNeural",  
+        "provider": "azure",
+        "speed": 1.12,
+        #"stability": 0.5,
+        #"similarityBoost": 0.75
+    }
+}
+
+def get_assistant(selected_template: str, region: str, company: str, candidate_name: str, role: str, questions: str):
+    recruiter = "Eric"
+    if region == 'India':
+        recruiter = "Tina"        
+    
+    first_bot_message = prompts.alt_first_bot_message.format(candidate_name=candidate_name)
+    if company.isalpha():
+        stt_model["keywords"].append(company)
+    
+    if candidate_name.isalpha():
+        stt_model["keywords"].append(candidate_name)
+    
+    print(stt_model["keywords"])
+    llm["messages"][0]["content"] = prompts.system_prompt.format(company=company)
+
+    if (selected_template == "Structural Coatings - Material Handler") or (
+        selected_template == "Hurricane Clean-Up Labor"
+    ):
+        llm['messages'][1]['content'] = prompts.user_prompt_material_handler.format(
+            recruiter=recruiter,
+            questions=questions, 
+            company=company,
+            role=role,
+        )
+    else:
+        llm['messages'][1]['content'] = prompts.user_prompt_with_probing.format(
+            questions=questions, 
+            company=company, 
+            role=role, 
+            recruiter=recruiter
+        )
+    
+        
+    voice_settings = REGION_VOICES.get(region, REGION_VOICES['US'])
+
+    vapi_assistant = {
+        "firstMessage": first_bot_message,
+        #"endCallMessage": prompts.end_call_message.format(candidate_name=candidate_name),
+        "endCallPhrases": ["Have a great day."],
+        "backgroundDenoisingEnabled": True,
+        "responseDelaySeconds": 0.8,
+        "silenceTimeoutSeconds": 30,
+        "transcriber": stt_model,
+        "model": llm,
+        "startSpeakingPlan": {
+            "waitSeconds": 1.0
+        },
+        "endCallFunctionEnabled": True,
+        "voice": voice_settings,
+        "backgroundSound": "off"
+    }
+
+    return vapi_assistant
